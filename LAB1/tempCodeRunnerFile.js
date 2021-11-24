e266f019a37a798cb85b6589e3077e651785b4ac
pipat000 <pbtonnnpipat@gmail.com>
2021-11-24 10:42:40 +0700

    first commit

---
+++ LAB1/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+const addEs6 = (a,b) => a * b;
+let multiAdd = (a,b) => {
+    let c = a * b;
+    return c;
+};
+
+console.log(addEs6(10,5));
+console.log(multiAdd(4,2));
---
+++ LAB2/es6-object.js
@@ -0,0 +1,54 @@
+let person ={
+    firstName : "Ton",
+    lastName : "Hae",
+    age : 21,
+    sex: "male",
+
+    fullName : function (){
+        return this.firstName + " " + this.lastName;
+    },
+};
+console.log(person);
+
+person.age = 20;
+console.log(person);
+
+const p = person;
+p.age =10;
+console.log(person.age); //value
+console.log(person.fullName); //property
+console.log(person.fullName()); //return
+
+///deatructuring
+const{firstName,lastName,age : a,sex : s} = person;
+console.log(firstName);
+console.log(s);
+
+ //string
+const S1 ="Hello World !!";
+const S2 = "My Name is";
+const Rname ="Ton";
+let conCat = S1 + S2 + Rname;
+console.log(conCat);
+conCat = `  I
+            love
+            it. ${S2} ${Rname}
+            `;
+console.log(conCat);
+
+//Spread op (...)
+const A1 = [2,3,4];
+const A2 = [5,6,7];
+let comArr= [A1, A2];
+console.log(comArr);
+
+comArr = [...A1, ...A2];
+console.log(comArr);
+
+sum =(...numbers) =>{
+    let total = "";
+    for (Value of numbers) total += Value;
+    return total;
+};
+
+console.log(sum(comArr));
