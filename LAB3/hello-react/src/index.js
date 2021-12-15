5b2a30bb9b921e8d936b43fc4911be1df0b98fc8
pipat000 <pbtonnnpipat@gmail.com>
2021-12-15 11:20:10 +0700

    LAB6

--- LAB3/hello-react/src/index.js
+++ LAB3/hello-react/src/index.js
@@ -6,12 +6,14 @@ import Car from "./components/Car";
 import List from "./components/List";
 import Ui from "./components/Ui";
 import Datatable from "./components/Table";
+import Hook from "./components/Hook";
 ReactDOM.render(
   <>
     <Datatable />
     <Car age="25" eye="black"/>
     <List />
     <Ui />
+    <Hook />
   </>
   ,document.getElementById("root")
   );
